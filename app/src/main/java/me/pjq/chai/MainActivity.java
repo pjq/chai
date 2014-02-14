6ca95b24b035922bd0510f0c0876b566fa3365c3
Jianqing Peng <pengjianqing@gmail.com>
2014-02-14 23:09:44 +0800

    design the icon, and update ui

--- app/src/main/java/me/pjq/chai/MainActivity.java
+++ app/src/main/java/me/pjq/chai/MainActivity.java
@@ -56,7 +56,6 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
 
         if (weChatUtils.getIWXAPI().isWXAppInstalled()) {
             getMenuInflater().inflate(R.menu.main_wechat, menu);
