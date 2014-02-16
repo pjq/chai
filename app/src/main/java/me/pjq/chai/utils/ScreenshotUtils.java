71d431b4d0f279b0ed6a9ff1870be194b13443b9
Jianqing Peng <pengjianqing@gmail.com>
2014-02-16 15:36:33 +0800

    add text2image

--- app/src/main/java/me/pjq/chai/utils/ScreenshotUtils.java
+++ app/src/main/java/me/pjq/chai/utils/ScreenshotUtils.java
@@ -114,7 +114,8 @@ public class ScreenshotUtils {
      */
     public static boolean drawTextToBitmap(Activity pActivity, String filePath, String text) {
         //return ScreenshotUtils.savePic(drawTextToBitmap(pActivity, R.drawable.bg, text), filePath);
-        return ScreenshotUtils.savePic(drawText(pActivity, R.drawable.bg, text), filePath);
+        //return ScreenshotUtils.savePic(drawText(pActivity, R.drawable.bg_grey, text), filePath);
+        return ScreenshotUtils.savePic(text2Bitmap(pActivity, R.drawable.bg_grey, text), filePath);
     }
 
     public static Bitmap drawTextToBitmap(Context gContext,
@@ -158,7 +159,6 @@ public class ScreenshotUtils {
 
     public static Bitmap drawText(Context gContext, int gResId, String gText) {
         Resources resources = gContext.getResources();
-        float scale = resources.getDisplayMetrics().density;
         Bitmap bitmap =
                 BitmapFactory.decodeResource(resources, gResId);
 
@@ -178,18 +178,39 @@ public class ScreenshotUtils {
         // text color - #3D3D3D
         paint.setColor(resources.getColor(R.color.text_black_color));
         // text size in pixels
-        float fontSize = Utils.dpToPixels(MyApplication.getContext(), 12);
+        float fontSize = Utils.dpToPixels(MyApplication.getContext(), 14);
 
         paint.setTextSize((int) (fontSize));
-        // text shadow
-        //paint.setShadowLayer(1f, 0f, 1f, Color.WHITE);
-
-        // draw text to the Canvas center
         Rect bounds = new Rect();
-        //paint.getTextBounds(gText, 0, gText.length(), bounds);
         bounds.set(0, 0, bitmap.getWidth() - 15, bitmap.getHeight() - 10);
 
         drawMultilineText(gText, 10, 30, paint, canvas, (int) fontSize, bounds);
+
+        return bitmap;
+    }
+
+    public static Bitmap text2Bitmap(Context gContext, int gResId, String gText) {
+        Resources resources = gContext.getResources();
+
+        Canvas canvas = new Canvas();
+        canvas.drawColor(resources.getColor(R.color.bg_color_light_grey));
+        // new antialised Paint
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(resources.getColor(R.color.text_black_color));
+        float fontSize = Utils.dpToPixels(MyApplication.getContext(), 14);
+
+        paint.setTextSize((int) (fontSize));
+        Rect bounds = new Rect();
+        int width = (int) (MyApplication.mScreenWidth * 0.7f);
+        int bottomPadding = 15;
+        int topPadding = 40;
+        int height = getMultilineTextHeight(gText, 10, topPadding, paint, canvas, (int) fontSize, width) + bottomPadding;
+        bounds.set(0, 0, width - 15, height - 10);
+        Bitmap bitmap = Bitmap.createBitmap(width, height, android.graphics.Bitmap.Config.ARGB_8888);
+
+        canvas.setBitmap(bitmap);
+
+        drawMultilineText(gText, 10, topPadding, paint, canvas, (int) fontSize, bounds);
 
         return bitmap;
     }
@@ -208,7 +229,6 @@ public class ScreenshotUtils {
 
             if (calculateWidthFromFontSize(line + "" + lines[i], fontSize) <= drawSpace.width()) {
                 line = line + "" + lines[i];
-
             } else {
                 canvas.drawText(line, x, y + yoffset, paint);
                 yoffset = yoffset + lineHeight;
@@ -216,6 +236,29 @@ public class ScreenshotUtils {
             }
         }
         canvas.drawText(line, x, y + yoffset, paint);
+    }
+
+    private static int getMultilineTextHeight(String str, int x, int y, Paint paint, Canvas canvas, int fontSize, int width) {
+        int lineHeight = 0;
+        int yoffset = 0;
+        String[] lines = str.split("");
+
+        // set height of each line (height of text + 20%)
+        float scale = 1.1f;
+        lineHeight = (int) (calculateHeightFromFontSize(str, fontSize) * scale);
+        // draw each line
+        String line = "";
+        for (int i = 0; i < lines.length; ++i) {
+            if (calculateWidthFromFontSize(line + "" + lines[i], fontSize) <= width) {
+                line = line + "" + lines[i];
+            } else {
+                canvas.drawText(line, x, y + yoffset, paint);
+                yoffset = yoffset + lineHeight;
+                line = lines[i];
+            }
+        }
+
+        return y + yoffset + lineHeight;
     }
 
     private static int calculateWidthFromFontSize(String testString, int currentSize) {
